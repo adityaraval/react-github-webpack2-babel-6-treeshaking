0eb9bc33f6567bbeec2ffdf4e3a90441e7ef7424
Aditya Raval <adi7017@gmail.com>
2017-08-15 20:11:29 +0530

    Added TaskList and simple APIs

--- server.js
+++ server.js
@@ -2,12 +2,47 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require('morgan')
+const {ObjectID} =  require('mongodb');
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./config/db');
+var {Task} = require('./model/task');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+});
+
+//post tasks
+app.post('/api/v1/tasks',(req,res)=>{
+  console.log(req.body);
+  var task = new Task({
+    text:"taskl",
+    completed:false
+  });
+
+  task.save().then((doc)=>{
+    res.send(doc);
+  },(err)=>{
+    res.status(400).send(err);
+  });
+});
+
+//get all tasks
+app.get('/api/v1/tasks',(req,res)=>{
+
+  Task.find().then((tasks)=>{
+    res.send({tasks:tasks});
+  },(err)=>{
+    res.status(400).send(err);
+  });
+});
+
+
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React,{Component} from 'react';
-
+import TaskBox from './TaskBox';
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          Hello World!
+        <div className="App container">
+          <div className="row vert-offset-top-2">
+            <div className="col-md-4" id="taskBox">
+              <TaskBox />
+            </div>
+          </div>
         </div>
     );
   }
